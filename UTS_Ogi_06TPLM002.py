5f1322c9c9d13e7c8a5b42ead57454dd8148fd6a
OgiOktofiardis <104136903+OgiOktofiardis@users.noreply.github.com>
2022-04-22 14:14:04 +0700

    Add files via upload

---
+++ UTS_Ogi_06TPLM002.py
@@ -0,0 +1,7 @@
+nama = input("Siapa nama anda? ")
+
+if nama=="OGI":
+    print("Nim saya Genap yaitu 191011400100 dan berarti benar OGI ")
+else:
+    print("Nim saya bukan Ganjil dan berarti bukan ogi ")
+print("Akhir dari program")
